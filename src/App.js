74577f618161867ee7ba91364e9bbcb77dad3d05
dut-a <d.dut@live.com>
2020-02-02 03:28:58 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+
+import React, {Component} from "react";
+
+export default class App extends Component {
+  state = {
+    dev: "Dut A.",
+    langs: ["JS", "Java", "Ruby"],
+    people: []
+  }
+
+  componentDidMount() {
+    let url = "http://api.open-notify.org/astros.json";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let p = data.people;
+        this.setState({people: p})
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        Asynchronous React ( <span style={{color: "red"}}>fetch()</span> in <span style={{color: "blue"}}>componentDidMount()</span> ).
+      </div>
+    );
+  }
+
+}
